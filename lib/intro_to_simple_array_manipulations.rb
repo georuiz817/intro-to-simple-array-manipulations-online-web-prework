319a27593917666bed8ce28ac891cd2bcbf4ff5b
George Ruiz <georuiz817@gmail.com>
2019-05-28 17:05:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,4 +46,5 @@ def using_delete(instructors, steven)
   no_offense_steven = instructors.delete(steven)
 end
   
-def using_delete_at( )
+def using_delete_at(famous_robots, 2 )
+  deleted_robot = famous_robots.delete(2)
