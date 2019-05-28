89d4cf491bbd5510dd648fc3eaac76c44909d133
George Ruiz <georuiz817@gmail.com>
2019-05-28 16:42:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,5 +26,5 @@ def shift_with_args(ice_cream_brands)
   brands_removed = ice_cream_brands.shift(2)
 end
 
-def using_concat(my_favorite_things, )
+def using_concat(my_favorite_things, more_favs)
   
