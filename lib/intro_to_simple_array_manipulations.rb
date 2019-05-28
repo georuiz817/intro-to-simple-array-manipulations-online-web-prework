1354735b08b6e09d872590ea941b13a489198b15
George Ruiz <georuiz817@gmail.com>
2019-05-28 16:59:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,7 +42,7 @@ def using_flatten(private_colleges_in_newyork)
   flat_array = private_colleges_in_newyork.flatten
 end
 
-def using_delete(instructors)
-  no_offense_steven = instructors.delete(2)
+def using_delete(instructors, "steven")
+  no_offense_steven = instructors.delete("steven")
 end
   
