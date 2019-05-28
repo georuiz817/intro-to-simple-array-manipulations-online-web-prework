bbfe475dfe27848f883995ed48a08fe95004cbd9
George Ruiz <georuiz817@gmail.com>
2019-05-28 16:30:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,15 +10,11 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
 
-`def using_pop(great_hits_of_the_nineties)
-great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-great_hits_of_the_nineties.pop
-end`
 
-def pop_with_args(chars_in_game_of_thrones)
+```def pop_with_args(chars_in_game_of_thrones)
   chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
   chars_arya_killed = chars_in_game_of_thrones.pop(2)
-end
+end```
 
 def using_shift(my_favorite_cities)
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
